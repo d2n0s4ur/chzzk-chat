bef7a6a629539c6377bf5dc35b5aabd9ca72a256
d2n0s4ur <njh0625@gmail.com>
2024-02-13 22:42:37 +0900

    :ambulance: Fix critical bug

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.chzzkChat = void 0;
-const chzzkChat_1 = require("../lib/chzzkChat");
-exports.chzzkChat = new chzzkChat_1.ChzzkChat("chzzkChannelId");
